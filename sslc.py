6fc418eb68a28b70fb137e3f939045b3249df544
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-06-03 22:41:51 +0530

    Update sslc.py

--- sslc.py
+++ sslc.py
@@ -1,18 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://karresults.nic.in/first_sslc_kar21.asp")
 
+driver.get("https://kseeb.karnataka.gov.in/sslcmainexamresults2022/")
 
-input=driver.find_element_by_xpath('//*[@id="InputRegno"]')
-input.send_keys('2021109991')
+input=driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_txtReg"]')
+input.send_keys('20220429543') #put your reg no
 
-#select day,month,year and leave it 
+#time.sleep(2)
 
-time.sleep(10)
+dropdown = Select(driver.find_element_by_id("ContentPlaceHolder1_ddlDay"))
+dropdown.select_by_value("27") #put your date of birth date
 
-button=driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/form/button').click()
+time.sleep(2)
+
+dropdown = Select(driver.find_element_by_id("ContentPlaceHolder1_ddlMonth"))
+dropdown.select_by_value("05") #put your date of birth month
+
+time.sleep(2)
+
+dropdown = Select(driver.find_element_by_id("ContentPlaceHolder1_ddlYear"))
+dropdown.select_by_value("2006") #put your date of birth year
+
+#time.sleep(2)
+
+input=driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_btnview"]').click()
+
+time.sleep(3)
+
 
 
 
